c25d43663120b5c7bf11daa778f6af37906ec176
24pwai0015-max <24pwai0015@uetpeshawar.edu.pk>
2026-07-02 04:38:14 -0700

    Week 3 Day 2 - groupby basics complete

--- pandas/w3_02_groupby_basics.py
+++ pandas/w3_02_groupby_basics.py
@@ -1,48 +1,48 @@
-# '''groupby :
-# Split data by category  →  Calculate something for each category  →  Show results'''
+'''groupby :
+Split data by category  →  Calculate something for each category  →  Show results'''
 
-# import pandas as pd
+import pandas as pd
 
-# df = pd.DataFrame({
-#     'Name': ['Ali', 'Sara', 'Ahmed', 'Fatima', 'Hassan'],
-#     'Class': ['A', 'B', 'A', 'B', 'A'],
-#     'Math': [45, 78, 90, 55, 67]
-# })
-# # print(df)
+df = pd.DataFrame({
+    'Name': ['Ali', 'Sara', 'Ahmed', 'Fatima', 'Hassan'],
+    'Class': ['A', 'B', 'A', 'B', 'A'],
+    'Math': [45, 78, 90, 55, 67]
+})
+# print(df)
 
-# # print(df.groupby('Class')['Math'].mean())
-# df['avg']=df.groupby('Class')['Math'].transform('mean')
-# # print(df)
-# '''In pandas, transform() is used to perform an operation on 
-# each group while keeping the original DataFrame shape. It returns
-# one value for each original row, unlike agg() which returns one value per group.'''
-# # print("Sum:\n",df.groupby('Class')['Math'].sum())     # total per class
-# # print("count:\n",df.groupby('Class')['Math'].count())    # how many students per class
-# # print("max:\n",df.groupby('Class')['Math'].max())      # highest score per class
-# # print("min:\n",df.groupby('Class')['Math'].min())      # lowest score per class
+# print(df.groupby('Class')['Math'].mean())
+df['avg']=df.groupby('Class')['Math'].transform('mean')
+# print(df)
+'''In pandas, transform() is used to perform an operation on 
+each group while keeping the original DataFrame shape. It returns
+one value for each original row, unlike agg() which returns one value per group.'''
+# print("Sum:\n",df.groupby('Class')['Math'].sum())     # total per class
+# print("count:\n",df.groupby('Class')['Math'].count())    # how many students per class
+# print("max:\n",df.groupby('Class')['Math'].max())      # highest score per class
+# print("min:\n",df.groupby('Class')['Math'].min())      # lowest score per class
 
-# #  Groupby on All Numeric Columns at Once
+#  Groupby on All Numeric Columns at Once
 
-# print("-"*50)
+print("-"*50)
 
-# # print(df.groupby('Class').mean(numeric_only=True))
-# # print(df.groupby('Class').mean(numeric_only=True))
+# print(df.groupby('Class').mean(numeric_only=True))
+# print(df.groupby('Class').mean(numeric_only=True))
 
-# df2 = pd.DataFrame({
-#     'Class': ['A', 'A', 'B', 'B'],
-#     'Gender': ['M', 'F', 'M', 'F'],
-#     'Math': [45, 78, 90, 55]
-# })
-# df2['math bonus']=df['Math'].apply(lambda x: x+5)
+df2 = pd.DataFrame({
+    'Class': ['A', 'A', 'B', 'B'],
+    'Gender': ['M', 'F', 'M', 'F'],
+    'Math': [45, 78, 90, 55]
+})
+df2['math bonus']=df['Math'].apply(lambda x: x+5)
+print(df2)
+# multiple groupby
+
+# df2['avg']=df2.groupby(['Class','Gender'])['Math'].transform('mean')
 # print(df2)
-# # multiple groupby
-
-# # df2['avg']=df2.groupby(['Class','Gender'])['Math'].transform('mean')
-# # print(df2)
 
 
-# # df3 = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
-# # print(df3.columns)
+# df3 = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
+# print(df3.columns)
 
 
 import pandas as pd
